830622d4a744b656e42fa5cca4f850f89967d12c
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2024-07-02 11:54:14 +0100

    add crud method in blog service

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -14,4 +14,20 @@ export class BlogService {
   _getAllArticles(): Observable<Blog[]> {
     return this.http.get<Blog[]>(this.apiUrl);
   }
+
+  _persistArticle(data: Blog): Observable<Blog> {
+    return this.http.post<Blog>(this.apiUrl, data);
+  }
+
+  _getOneArticle(id: number | string): Observable<Blog> {
+    return this.http.get<Blog>(`${this.apiUrl}/${id}`);
+  }
+
+  _updateArticle(id: number | string, data: Blog): Observable<Blog> {
+    return this.http.put<Blog>(`${this.apiUrl}/${id}`, data);
+  }
+
+  _deleteArticle(id: number | string): Observable<Blog> {
+    return this.http.delete<Blog>(`${this.apiUrl}/${id}`);
+  }
 }
